1c655341f6e9fad7a446aa82135b26d2afacccfc
Patric Gekoski <=>
2025-01-20 17:42:55 -0500

    Setup page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
 	content: ['./*.html'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				lightGreen: 'hsl(148, 38%, 91%)',
+				darkGreen: 'hsl(169, 82%, 27%)',
+				error: 'hsl(0, 66%, 54%)',
+				lightGray: 'hsl(186, 15%, 59%)',
+				darkGray: 'hsl(187, 24%, 22%)',
+			},
+		},
 	},
 	plugins: ['prettier-plugin-tailwindcss'],
 }
