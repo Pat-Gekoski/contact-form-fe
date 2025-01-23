6745288cc0023f21e8782d2c7c4635063354728f
Patric Gekoski <=>
2025-01-22 19:56:25 -0500

    Start form validation setup

--- js/script.js
+++ js/script.js
@@ -1,4 +1,31 @@
 const radioBtns = document.querySelectorAll('.radio-input')
+const firstNameInput = document.getElementById('firstsName')
+const lastNameInput = document.getElementById('lastName')
+const emailInput = document.getElementById('email')
+const messageInput = document.getElementById('message')
+
+const formState = {
+	firstName: {
+		value: '',
+		touched: false,
+		error: '',
+	},
+	lastName: {
+		value: '',
+		touched: false,
+		error: '',
+	},
+	email: {
+		value: '',
+		touched: false,
+		error: '',
+	},
+	message: {
+		value: '',
+		touched: false,
+		error: '',
+	},
+}
 
 radioBtns.forEach((input) => {
 	input.addEventListener('change', () => {
